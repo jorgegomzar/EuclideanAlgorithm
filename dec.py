474af57d5e21af5370e338c7ff2e16ed09e69e68
jorgegomzar <jorge.gomzar@gmail.com>
2018-09-25 23:50:54 +0200

    Añadidas nuevas funcionalidades

---
+++ dec.py
@@ -0,0 +1,34 @@
+def descifrar(n, d):
+
+	cad = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	dic = {}
+	listOut = []
+
+	for k, v in enumerate(cad, start=0):
+			dic[v] = k
+	cadIn = input('Introduce caracteres: ').upper()
+	listC = [c for c in cadIn]
+
+	print('Computando...\n')
+
+	listNum = [dic[c] for c in listC]
+
+	dic = dict((v, k) for k, v in dic.items())
+
+	for i in listNum:
+		listOut.append(dic[listNum[i]**d % n])
+	return listOut
+
+def num2letra():
+	cad = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	dic = {}
+	listOut = []
+
+	for k, v in enumerate(cad, start=0):
+			dic[k] = v
+	cIn = input('Número: ')
+	while cIn != '':
+		cIn = int(cIn)
+		listOut.append(dic[cIn])
+		cIn = input('Número: ')
+	print(''.join(listOut))
--- script.py
+++ script.py
@@ -1,4 +1,16 @@
-phi = int(input('PHI: '))
+import os, dec
+from sys import platform
+
+def clear():
+	if platform == "linux":
+		_ = os.system('clear')
+	elif platform == "win32":
+		_ = os.system('cls')
+
+p = int(input('p: '))
+q = int(input('q: '))
+n = p * q
+phi = (p-1)*(q-1)
 e = int(input('e: '))
 print('Calculating d...')
 
@@ -12,4 +24,17 @@ while restos[-1] != 1:
 	cocientes.append(restos[i]//restos[i+1])
 	posD.append((posD[i]-posD[i+1]*cocientes[i])%phi)
 	i = i + 1
+print('restos: ',restos)
+print('cocientes: ',cocientes)
+print('posD: ',posD)
 print('d =',posD[-1])
+
+os.system('pause')
+clear()
+if input('¿Descifrar? (s/n): ').lower() == 's':
+	d = posD[-1]
+	if input('¿Pasar de número a cadena? (s/n): ').lower() == 's':
+		dec.num2letra()
+	listDescifrada = dec.descifrar(n, d)
+	print('Cadena descifrada: '+''.join(listDescifrada))
+ 
