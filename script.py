46bf3d7e606b399108a86348a226ee48521ac529
jorgegomzar <jorge.gomzar@gmail.com>
2018-09-26 13:39:31 +0200

    translated and commented

--- script.py
+++ script.py
@@ -1,17 +1,14 @@
-# Starting with the english version of the project
-
 import os, dec
 from sys import platform
 
 def clear():
-	"""Función para limpiar la pantalla del terminal
-	tanto en linux como en windows"""
+	""" Multi S.O. function to clear the terminal screen """
 	if platform == "linux":
 		_ = os.system('clear')
 	elif platform == "win32":
 		_ = os.system('cls')
 
-print('Introduce los siguientes valores')
+print('Complete the requested values')
 p = int(input('p: '))
 q = int(input('q: '))
 n = p * q
@@ -20,31 +17,35 @@ e = int(input('e: '))
 print('Calculating d...')
 
 posD = [phi, 1]
-restos = [phi, e]
-cocientes = []
+rem = [phi, e]
+quot = []
 # ------------------------------- #
-# ALGORITMO EXTENDIDO DE EUCLIDES #
+#   EXTENDED ECLUDEAN ALGORITHM   #
 # ------------------------------- #
-i = 0 														# Indice inicial de RESTOS y POSD
-while restos[-1] != 1 and restos[-1] != 0:					# Que repita lo siguiente mientras que el el último resto no sea 1
-	restos.append(restos[i]%restos[i+1])					# Se añade a RESTOS, el resto de la división entre los dos últimos elementos del vector RESTOS
-	cocientes.append(restos[i]//restos[i+1])				# Se añade a COCIENTES, el cociente de la división entre los dos últimos elementos del vector RESTOS
-	posD.append((posD[i]-posD[i+1]*cocientes[i])%phi)		# Se añade a POSD, la resta entre el penúltimo elemento del vector POSD y el producto del último del vector POSD por el último cociente
-# DEBUGGING
-# print('restos: ',restos)
-# print('cocientes: ',cocientes)
+i = 0 														# Initial index
+while rem[-1] != 1 and rem[-1] != 0:						# While remainder is not 1 or 0 keep going
+	rem.append(rem[i]%rem[i+1])								# Append to REM the remainder of the division of the last two remainders
+	quot.append(rem[i]//rem[i+1])							# Append to QUOT the quotient of the last division
+	posD.append((posD[i]-posD[i+1]*quot[i])%phi)			# Append to POSD the penultimate posD minus the product between the last posD and the last quotient
+
+# For debugging purposes
+# print('rem: ',rem)
+# print('quot: ',quot)
 # print('posD: ',posD)
-if restos[-1] != 0:
+
+if rem[-1] != 0:											# If the last remainder is equal to 0 means that some number is not right
 	print('d =',posD[-1])
 else:
-	print('No se ha podido hallar d, comprueba todos los parámetros introducidos.')
+	print('Couldn\'t find d, check all introduced parameters.')
+	exit()
+
 
 os.system('pause')
 clear()
 
-if input('¿Descifrar? (s/n): ').lower() == 's':
+if input('Decrypt? (s/n): ').lower() == 's':
 	d = posD[-1]
-	cad = input('Introduce la cadena a desencriptar: ')
-	listDescifrada = dec.descifrar(cad, n, d)
-	print('Cadena descifrada: '+''.join(listDescifrada))
+	cad = input('Insert the string to decrypt: ')
+	listDescifrada = dec.descifrar(cad, n, d)				# Decrypt the string
+	print('Cadena descifrada: '+''.join(listDescifrada))	# Show it via terminal
  
